9fedf2d00979dc1520f8997c28aa84261719b799
Pooya <pooya.darvehei@gmail.com>
2025-03-29 15:37:30 +0000

    Connect ProjectPage to API to fetch data

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -4,7 +4,8 @@ import { Button, message, Spin } from 'antd';
 import ProjectDetailsSection from '../components/ProjectDetailsSection';
 import TicketsSection from '../components/TicketsSection';
 import FabricsSection from '../components/FabricsSection';
-
+import { getChangedObjects } from '../utils';
+const url = 'https://8ap1z6w9bb.execute-api.ap-southeast-1.amazonaws.com/prod/projects/'
 const ProjectPage = () => {
   const { projectId } = useParams();
   const [project, setProject] = useState(null);
@@ -14,7 +15,10 @@ const ProjectPage = () => {
   useEffect(() => {
     const fetchProject = async () => {
       try {
-        const response = await fetch('/sample_project.json');
+        const response = await fetch(url + projectId);
+        if (!response.ok) {
+          throw new Error('Invalid response from server');
+        }
         const project = await response.json();
         setProject(project);
         setEditedProject(project);
@@ -26,6 +30,7 @@ const ProjectPage = () => {
     fetchProject();
   }, [projectId]);
 
+  
   const handleTicketChange = (key, value, record) => {
     const newTickets = editedProject.tickets.map(ticket => {
       if (ticket.id === record.id) {
@@ -69,19 +74,18 @@ const ProjectPage = () => {
   const handleUpdateProject = async () => {
     setIsLoading(true);
     try {
-    //   const response = await fetch('/api/projects/' + projectId, {
-    //     method: 'PUT',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(editedProject),
-    //   });
-      
-    //   if (!response.ok) {
-    //     throw new Error('Failed to update project');
-    //   }
-      await new Promise(resolve => setTimeout(resolve, 3000));
+      // await new Promise(resolve => setTimeout(resolve, 3000));
       setProject(editedProject);
+      const changedTickets = getChangedObjects(project.tickets, editedProject.tickets);
+      const changedFabrics = getChangedObjects(project.fabrics, editedProject.fabrics);
+      if (changedTickets.length === 0 && changedFabrics.length === 0) {
+        console.log('No changes made to the project');
+        setIsLoading(false);
+        return;
+      }
+      
+      console.log('Changed Tickets:', changedTickets);
+      console.log('Changed Fabrics:', changedFabrics);
       message.success('Project updated successfully');
     } catch (error) {
       message.error('Failed to update project');
